1744d10e7bd7f68a675d2d67f09f4b06b86c2b33
arielgzdigital <arielgzdigital@gmail.com>
2025-12-13 22:47:03 -0300

    Configure ALLOWED_HOSTS for Render deployment
    Update ALLOWED_HOSTS for deployment on Render.

--- rcianatural/settings/local.py
+++ rcianatural/settings/local.py
@@ -6,7 +6,12 @@ SECRET_KEY = 'django-insecure-$4-5#3mt(57ic8-q$=d3x42wud&!lrg7k=#u1jkn556cqjp(sp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# PARA QUE FUNCIONE EN RENDER
+ALLOWED_HOSTS = [
+    'resistencianatural.onrender.com', 
+    'localhost', 
+    '127.0.0.1'
+]
 
 DATABASES = {
     'default': {
